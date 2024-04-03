d43611e3bb3dc907d33403ebd0cecfd934d5923b
DasNunes <joaopedrolinomoreira@gmail.com>
2024-04-03 16:38:49 -0300

    first commit

---
+++ js/scritp.js
@@ -0,0 +1,7 @@
+function exibirJogadoresNaTela() {
+    var corpoDaTabela = document.getElementById('corpoDaTabela')  
+    var linhaDaTabela = "<tr><td>Paulo</td><td></td><td></td><td></td><td></td><td></td></tr>"
+    
+    corpoDaTabela.innerHTML = linhaDaTabela
+}   
+exibirJogadoresNaTela()
